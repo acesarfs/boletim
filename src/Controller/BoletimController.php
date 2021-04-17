8dd2be6ab1dde7a668d275997b3c3c27042d1bcf
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-04-16 22:41:02 -0300

    multiple to

--- src/Controller/BoletimController.php
+++ src/Controller/BoletimController.php
@@ -35,34 +35,34 @@ class BoletimController extends ControllerBase {
 
   public function email($node) {
 
-    $mailManager = \Drupal::service('plugin.manager.mail');
+    $tos = ['eventosdf_fflch@listas.usp.br','fflch@listas.usp.br'];
 
-    $module = 'boletim';
-    $key = 'boletim_key';
-    $to = 'fflchsti@usp.br';
-    #$to = \Drupal::config('system.site')->get('mail');
-    $params['subject'] = $node->title->value;
-    $params['message'] = $node->body->value;
-    $langcode = \Drupal::currentUser()->getPreferredLangcode();
-    $reply = \Drupal::config('system.site')->get('mail');
-    $send = true;
+    foreach($tos as $to) {
+      $mailManager = \Drupal::service('plugin.manager.mail');
+      $module = 'boletim';
+      $key = 'boletim_key';
+      $params['subject'] = $node->title->value;
+      $params['message'] = $node->body->value;
+      $langcode = \Drupal::currentUser()->getPreferredLangcode();
+      $reply = \Drupal::config('system.site')->get('mail');
+      $send = true;
 
-    $result = $mailManager->mail($module, $key, $to, $langcode, $params, $reply , $send);
+      $result = $mailManager->mail($module, $key, $to, $langcode, $params, $reply , $send);
     
-    if ($result['result'] != true) {
-      $message = t('There was a problem sending your email notification to @email.', array('@email' => $to));
-      drupal_set_message($message, 'error');
-      \Drupal::logger('mail-log')->error($message);
-    } else {
-      $message = t('An email notification has been sent to @email ', array('@email' => $to));
-      drupal_set_message($message);
-      \Drupal::logger('mail-log')->notice($message);
-    }
+      if ($result['result'] != true) {
+        $message = t('There was a problem sending your email notification to @email.', array('@email' => $to));
+        drupal_set_message($message, 'error');
+        \Drupal::logger('boletim')->error($message);
+      } else {
+        $message = t('An email notification has been sent to @email ', array('@email' => $to));
+        drupal_set_message($message);
+        \Drupal::logger('boletim')->notice($message);
+      }
   
-    $url = Url::fromRoute('entity.node.edit_form', ['node' => $node->nid->value])->toString();
-    $response = new RedirectResponse($url);
-    $response->send();
-
+      $url = Url::fromRoute('entity.node.edit_form', ['node' => $node->nid->value])->toString();
+      $response = new RedirectResponse($url);
+      $response->send();
+    }  
   }
 }
 
